6d411008701588bdfba0c37397b0755125683cb8
Lixue Cheng <lcheng2@caltech.edu>
2025-10-16 11:00:52 +0800

    update tests

--- main.py
+++ main.py
@@ -364,6 +364,10 @@ def emulator():
     data = _data()
     return render_template("emulator.html", **data)
 
+@app.route("/sampler.html")
+def sampler():
+    data = _data()
+    return render_template("sampler.html", **data)
 
 @app.route("/predictor.html")
 def predictor():
